f224e3d0cfa4efd7a58b2c4c7b57bdd2926cff8c
M K <mkpro118@gmail.com>
2025-12-12 16:38:07 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![forbid(clippy::allow_attributes)]
+#![forbid(clippy::complexity)]
+#![forbid(clippy::correctness)]
+#![forbid(clippy::expect_used)]
+#![forbid(clippy::pedantic)]
+#![forbid(clippy::perf)]
+#![forbid(clippy::style)]
+#![forbid(clippy::suspicious)]
+#![forbid(clippy::unwrap_used)]
+#![forbid(future_incompatible)]
+#![forbid(unsafe_code)]
+
+fn main() {
+    todo!();
+}
